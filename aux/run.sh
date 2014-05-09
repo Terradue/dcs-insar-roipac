d06144f07c1ce9fd2aa73134adc20b218883af81
Fabrice Brito <fabrice.brito@terradue.com>
2014-05-09 16:58:32 +0200

    Update run.sh

--- aux/run.sh
+++ aux/run.sh
@@ -8,7 +8,7 @@ source ${ciop_job_include}
 SUCCESS=0
 ERR_AUX=2
 ERR_VOR=4
-
+#export HOME=/tmp
 # add a trap to exit gracefully
 function cleanExit ()
 {
@@ -31,12 +31,17 @@ cat_osd_root="`ciop-getparam aux_catalogue`"
 function getAUXref() {
   local rdf=$1
   local ods=$2
-
+  ciop-log "INFO" "rdf is $rdf"
+  ciop-log "INFO" "ods is $ods" 
   startdate="`ciop-casmeta -f "ical:dtstart" $rdf | tr -d "Z"`"
   stopdate="`ciop-casmeta -f "ical:dtend" $rdf | tr -d "Z"`"
+  ciop-log "INFO" "startdate is $startdate"
+  ciop-log "INFO" "stopdate is $stopdate"
+  ciop-log "INFO" "opensearch-client -f Rdf -p time:start=$startdate -p time:end=$stopdate $ods"
 
   opensearch-client -v -f Rdf -p "time:start=$startdate" -p "time:end=$stopdate" $ods
 }
+
 
 
 while read input
