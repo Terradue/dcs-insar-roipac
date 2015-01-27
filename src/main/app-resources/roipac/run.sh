3b7620a3593f9ce7d1356f70f31229b1e39d3747
fdandria <fdandria@sb-10-16-10-27.dev.terradue.int>
2015-01-27 17:57:42 +0100

    [master] fixed some bugs in the roipac run.sh

--- src/main/app-resources/roipac/run.sh
+++ src/main/app-resources/roipac/run.sh
@@ -14,27 +14,31 @@ ERR_NODEM=7
 # add a trap to exit gracefully
 function cleanExit ()
 {
-local retval=$?
-local msg=""
-case "$retval" in
-$SUCCESS) msg="Processing successfully concluded";;
-$ERR_AUX) msg="Failed to retrieve auxiliary products";;
-$ERR_VOR) msg="Failed to retrieve orbital data";;
-$ERR_INVALIDFORMAT) msg="Invalid format must be roi_pac or gamma";;
-$ERR_NOIDENTIFIER) msg="Could not retrieve the dataset identifier";;
-$ERR_NODEM) msg="DEM not generated";;
-*) msg="Unknown error";;
-esac
-[ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
-exit $retval
+    local retval=$?
+    local msg=""
+    case "$retval" in
+        $SUCCESS) msg="Processing successfully concluded";;
+        $ERR_AUX) msg="Failed to retrieve auxiliary products";;
+        $ERR_VOR) msg="Failed to retrieve orbital data";;
+        $ERR_INVALIDFORMAT) msg="Invalid format must be roi_pac or gamma";;
+        $ERR_NOIDENTIFIER) msg="Could not retrieve the dataset identifier";;
+        $ERR_NODEM) msg="DEM not generated";;
+        *) msg="Unknown error";;
+    esac
+
+    [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
+    exit $retval
 }
+
 trap cleanExit EXIT
 
 # create a shorter TMPDIR name for some ROI_PAC scripts/binaires 
 UUIDTMP="/tmp/`uuidgen`"
-ln -s $TMPDIR $UUIDTMP
-
+#ln -s $TMPDIR $UUIDTMP
+mkdir ${UUIDTMP}
 export TMPDIR=$UUIDTMP
+
+ciop-log "DEBUG" "working in tmp dir [${TMPDIR}]"
 
 # prepare ROI_PAC environment variables
 export INT_BIN=/usr/bin/
@@ -48,47 +52,39 @@ export INS_DIR=$SAR_ENV_ORB
 cat > $TMPDIR/input
 
 # retrieve the aux files
-
 mkdir -p $TMPDIR/aux
 for input in `cat $TMPDIR/input | grep 'aux='` 
 do
-  echo ${input#aux=} | ciop-copy -O $TMPDIR/aux -
-
-  res=$?
-  
-  [ $res != 0 ] && exit $ERR_AUX
+    ciop-log "DEBUG" "retrieving aux file ${input#aux=}"
+    echo ${input#aux=} | ciop-copy -O $TMPDIR/aux - 2> /dev/null
+    [ $? -ne 0 ] && exit $ERR_AUX
 done
 
 # retrieve the orbit data
 mkdir -p $TMPDIR/vor
 for input in `cat $TMPDIR/input | grep 'vor='` 
 do
-  echo ${input#vor=} | ciop-copy -O $TMPDIR/vor -
-
-  res=$?
-  
-  [ $res != 0 ] && exit $ERR_VOR
+    ciop-log "DEBUG" "retrieving orbit file ${input#aux=}"
+    echo ${input#vor=} | ciop-copy -O $TMPDIR/vor - 2> /dev/null
+    [ $? -ne 0 ] && exit $ERR_VOR
 done
 
 # retrieve the DEM
 mkdir -p $TMPDIR/workdir/dem
-
-cat $TMPDIR/input
-ciop-log "INFO" "PAUSE"
-
-dem_wps_result_xml=`cat $TMPDIR/input | egrep -v '(aux=|vor=|sar=)'`
+wps_result="$( ciop-browseresults -r ${CIOP_WF_RUN_ID} -j node_dem -w | tr -d '\r' | tr -d '\n')"
+ciop-log "DEBUG" "dem wps results is ${wps_result}z"
 
 # extract the result URL
-ciop-log "INFO" "ciop-copy $dem_wps_result_xml | xsltproc /application/roipac/xslt/getresult.xsl - | xsltproc /application/roipac/xslt/metalink.xsl - | grep http | xargs -i curl -L -s {} -o $TMPDIR/workdir/dem/dem.tgz"
+curl -L -o $TMPDIR/workdir/dem/dem.tgz "${wps_result}" 2> /dev/null
 #curl -L -s $dem_wps_result_xml | xsltproc /application/roipac/xslt/getresult.xsl - | xsltproc /application/roipac/xslt/metalink.xsl - | grep http | xargs -i curl -L -s {} -o $TMPDIR/workdir/dem/dem.tgz
-wps_result=`ciop-copy $dem_wps_result_xml`
+#wps_result=`ciop-copy $dem_wps_result_xml`
 
 # workaround for spurious bytes in the response 
-tgz_metalink=`cat $wps_result | xsltproc /application/roipac/xslt/getresult.xsl -`
+#tgz_metalink=`cat $wps_result | xsltproc /application/roipac/xslt/getresult.xsl -`
 
-curl -L -s $tgz_metalink | xsltproc /application/roipac/xslt/metalink.xsl - | grep http | xargs -i curl -L -s {} -o $TMPDIR/workdir/dem/dem.tgz
+#curl -L -s $tgz_metalink | xsltproc /application/roipac/xslt/metalink.xsl - | grep http | xargs -i curl -L -s {} -o $TMPDIR/workdir/dem/dem.tgz
 
-mkdir $TMPDIR/workdir/dem/
+#mkdir $TMPDIR/workdir/dem/
 
 tar xzf $TMPDIR/workdir/dem/dem.tgz -C $TMPDIR/workdir/dem/ 
 
@@ -101,40 +97,42 @@ roipac_proc=$TMPDIR/workdir/roi_pac.proc
 
 for input in `cat $TMPDIR/input | grep 'sar='`
 do
-  sar_url=`echo $input | cut -d "=" -f 2`
+    sar_url=`echo $input | cut -d "=" -f 2`
 
-  # get the date in format YYMMDD
-  sar_date=`ciop-casmeta -f "ical:dtstart" $sar_url | cut -c 3-10 | tr -d "-"`
-  sar_date_short=`echo $sar_date | cut -c 1-4`
-  ciop-log "INFO" "SAR date: $sar_date and $sar_date_short"
-
-  # get the dataset identifier
-  sar_identifier=`ciop-casmeta -f "dc:identifier" $sar_url`
-  ciop-log "INFO" "SAR identifier: $sar_identifier"
-
-  sar_folder=$TMPDIR/workdir/$sar_date
-  mkdir -p $sar_folder
+    # get the date in format YYMMDD
+    sar_date=`ciop-casmeta -f "ical:dtstart" $sar_url | cut -c 3-10 | tr -d "-"`
+    sar_date_short=`echo $sar_date | cut -c 1-4`
   
-  # get ASAR products
-  sar="`ciop-copy -o $sar_folder $sar_url`"
+    ciop-log "DEBUG" "SAR input ${sar_url}"
+    ciop-log "INFO" "SAR date: $sar_date and $sar_date_short"
 
-  cd $sar_folder
-  ciop-log "DEBUG" "make_raw_envi.pl $sar_identifier DOR $sar_date"
-  make_raw_envi.pl $sar_identifier DOR $sar_date 1>&2
+    # get the dataset identifier
+    sar_identifier=`ciop-casmeta -f "dc:identifier" $sar_url`
+    ciop-log "INFO" "SAR identifier: $sar_identifier"
 
-  if [ ! -e "$roipac_proc" ]
-  then
-    echo "SarDir1=$sar_date" > $roipac_proc
-    intdir="$sar_date"
-    sar1="$sar_date"
-    geodir="geo_${sar_date_short}"
-  else
-    echo "SarDir2=$sar_date" >> $roipac_proc
-    intdir=${intdir}-${sar_date}
-    sar2="$sar_date"
-    base=${sar1}_${sar2}
-    geodir=${geodir}-${sar_date_short}
-  fi
+    sar_folder=$TMPDIR/workdir/$sar_date
+    mkdir -p $sar_folder
+  
+    # get ASAR products
+    sar="`ciop-copy -o $sar_folder $sar_url`"
+
+    cd $sar_folder
+    ciop-log "DEBUG" "make_raw_envi.pl $sar_identifier DOR $sar_date"
+    make_raw_envi.pl $sar_identifier DOR $sar_date 1>&2
+
+    if [ ! -e "$roipac_proc" ]
+    then
+        echo "SarDir1=$sar_date" > $roipac_proc
+        intdir="$sar_date"
+        sar1="$sar_date"
+        geodir="geo_${sar_date_short}"
+    else
+        echo "SarDir2=$sar_date" >> $roipac_proc
+        intdir=${intdir}-${sar_date}
+        sar2="$sar_date"
+        base=${sar1}_${sar2}
+        geodir=${geodir}-${sar_date_short}
+    fi
 done 
 
 ciop-log "INFO" "Conversion of SAR pair to RAW completed"
@@ -181,10 +179,7 @@ ciop-log "INFO" "Invoking ROI_PAC process_2pass"
 
 cd $TMPDIR/workdir
 process_2pass.pl $roipac_proc 1>&2
-
-res=$?
-
-[ $res != 0 ] && exit $ERR_PROCESS2PASS
+[ $? -ne 0 ] && exit $ERR_PROCESS2PASS
 
 cd int_${intdir}
 
@@ -201,15 +196,12 @@ gdal_translate NETCDF:"geo_${intdir}.int.nc":magnitude geo_${intdir}.int.magnitu
 gdal_translate NETCDF:"geo_${intdir}.unw.nc":phase geo_${intdir}.unw.phase.tif
 
 ciop-log "INFO" "Publishing results"
-
 ciop-publish -m $TMPDIR/workdir/*.proc
 
 ciop-log "INFO" "Publishing baseline file"
-
 ciop-publish -m $TMPDIR/workdir/int_${intdir}/*_baseline.rsc
 
 ciop-log "INFO" "Publishing multi-look interferograms"
-
 ciop-publish -m $TMPDIR/workdir/int_${intdir}/${intdir}-sim*.int
 ciop-publish -m $TMPDIR/workdir/int_${intdir}/${intdir}-sim*.int.rsc
 
@@ -227,8 +219,8 @@ ciop-publish -m $TMPDIR/workdir/int_${intdir}/log1
 
 for file in `find . -name "${intdir}*.cor"`
 do
-ciop-publish -m $TMPDIR/workdir/int_${intdir}/$file
-  ciop-publish -m $TMPDIR/workdir/int_${intdir}/$file.rsc
+    ciop-publish -m $TMPDIR/workdir/int_${intdir}/$file
+    ciop-publish -m $TMPDIR/workdir/int_${intdir}/$file.rsc
 done
 
 ciop-log "INFO" "Publishing tif files"
@@ -241,6 +233,6 @@ ciop-publish -m $TMPDIR/workdir/int_${intdir}/$intdir.int
 ciop-publish -m $TMPDIR/workdir/int_${intdir}/$intdir.int.rsc
 
 
-rm -fr $UUIDTMP
+#rm -fr $UUIDTMP
 
 ciop-log "INFO" "That's all folks"
